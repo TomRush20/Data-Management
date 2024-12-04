45cf1096c4818b6bbbec8966013c726f01c08d06
TomRush20 <rushtw24@wfu.edu>
2024-12-03 20:36:34 -0500

    Fixed Dataset Display

--- Streamlit_Ebay_Assignment.py
+++ Streamlit_Ebay_Assignment.py
@@ -8,13 +8,22 @@ st.header('Ebay Data by Brand')
 st.divider()
 
 unique_brands = ebay_cleaned_df['Brand'].unique()
-unique_os = ebay_cleaned_df['OS'].unique()
 
-brand = st.selectbox(label='Select a Brand', options=unique_brands)
-OS = st.radio(label='Select color', options=unique_os)
+brand = st.multiselect(label='Select a Brand', options=unique_brands)
+price = st.radio(label='Select Price Range', options=['Cheap', 'Medium', 'Expensive'])
 
-if OS:
-    filtered = ebay_cleaned_df[(ebay_cleaned_df['Brand'] == brand) & (ebay_cleaned_df['OS'] == OS)]
-    st.dataframe(filtered, use_container_width=True)
+lower = 0
+upper = 0
+if price == 'Cheap':
+    lower = 0
+    upper = 500
+elif price == 'Medium':
+    lower = 500
+    upper = 1000
 else:
-    st.write('Choose an OS')
+    lower = 1000
+    upper = 3000
+
+filtered = ebay_cleaned_df[(ebay_cleaned_df['Brand'].isin(brand)) & ((ebay_cleaned_df['Price'] >= lower) & 
+                           (ebay_cleaned_df['Price'] < upper))]
+st.dataframe(filtered, use_container_width=True)
